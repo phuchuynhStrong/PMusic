747052ffe07d2219337f248e1c9b9305bc364234
Phuc Huynh <phuchuynh.strong@gmail.com>
2018-04-30 18:27:22 +0700

    Update login when navigating from splash to main

--- app/src/main/java/phucht/com/pmusic/UI/SplashScr.java
+++ app/src/main/java/phucht/com/pmusic/UI/SplashScr.java
@@ -1,5 +1,7 @@
 package phucht.com.pmusic.UI;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -8,6 +10,7 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 import com.wang.avi.AVLoadingIndicatorView;
 
+import phucht.com.pmusic.MainActivity;
 import phucht.com.pmusic.R;
 
 public class SplashScr extends AppCompatActivity {
@@ -15,6 +18,7 @@ public class SplashScr extends AppCompatActivity {
     ImageView mBackground;
     ImageView mLogo;
     AVLoadingIndicatorView mIndicator;
+    Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +43,23 @@ public class SplashScr extends AppCompatActivity {
     private void startLoading() {
         mIndicator.show();
 
-
-        stopLoading();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        stopLoading();
+                    }
+                });
+            }
+        }, 2000);
     }
 
     private void stopLoading() {
+        Intent intent = new Intent(this, MainActivity.class);
         mIndicator.hide();
+        startActivity(intent);
+        finish();
     }
 }
