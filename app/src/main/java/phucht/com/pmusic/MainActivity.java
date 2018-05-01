a45a5784376e334c1ab56c205a93b1e330e62fba
Phuc Huynh <phuchuynh.strong@gmail.com>
2018-05-01 15:58:59 +0700

    Add blur lib, player screen ui

--- app/src/main/java/phucht/com/pmusic/MainActivity.java
+++ app/src/main/java/phucht/com/pmusic/MainActivity.java
@@ -1,13 +1,36 @@
 package phucht.com.pmusic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import phucht.com.pmusic.UI.PlayerScreen;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mHeader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHeader = findViewById(R.id.activity_main_header_rnb);
+
+        mHeader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playerScreenNavigate(null);
+            }
+        });
+    }
+
+    private void playerScreenNavigate(JSONObject dataObj) {
+        Intent intent = new Intent(this, PlayerScreen.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/phucht/com/pmusic/UI/PlayerScreen.java
@@ -0,0 +1,15 @@
+package phucht.com.pmusic.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import phucht.com.pmusic.R;
+
+public class PlayerScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_screen);
+    }
+}
