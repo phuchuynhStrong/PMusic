292b0a92f64c0850ac1c6dde0f4f659b08bcb3f3
Phuc Huynh <phuchuynh.strong@gmail.com>
2018-05-06 08:04:42 +0700

    player migrate

--- app/src/main/java/phucht/com/pmusic/UI/PlayerScreen.java
+++ app/src/main/java/phucht/com/pmusic/UI/PlayerScreen.java
@@ -1,5 +1,6 @@
 package phucht.com.pmusic.UI;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +8,13 @@ import phucht.com.pmusic.R;
 
 public class PlayerScreen extends AppCompatActivity {
 
+    ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_screen);
+
+        mViewPager = findViewById(R.id.view_pager);
     }
 }
